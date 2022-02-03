3d74d0627aa2a21d386089d0ec3421668c02fa5b
ritwik <ritwikmads@gmail.com>
2022-02-03 12:02:28 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        Version: 1.63.2 (user setup) Date: 2021-12-15T09:40:02.816Z Electron:
+        13.5.2 Chromium: 91.0.4472.164 Node.js: 14.16.0 V8:
+        9.1.269.39-electron.0 OS: Windows_NT x64 10.0.19043
+      </p>
+    </div>
+  );
+}
+
+export default App;
